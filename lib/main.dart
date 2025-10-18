5ffa8df656b633efda25a74d146ca7b7a2daf08b
wujiseon <thienngo.tech@gmail.com>
2025-10-18 15:49:15 +0700

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Neura App',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+      ),
+      home: Scaffold(
+        body: Center(
+          child: Text('Neura app'),
+        ),
+      ),
+    );
+  }
+}
+
